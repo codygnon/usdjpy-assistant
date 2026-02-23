8d4abb0fc3844cf0ac39c9c8e2c8831674f146ae
codygnon <codygnon1680@gmail.com>
2026-02-22 22:58:29 -0500

    pulback fix

--- core/execution_engine.py
+++ core/execution_engine.py
@@ -5031,9 +5031,9 @@ def _evaluate_m3_slope_trend_trial_6(
     all_slopes_neg = slope_fast < 0 and slope_slow < 0 and slope_extra < 0
 
     trend = "NONE"
-    if bull_stack and all_slopes_pos:
+    if bull_stack:
         trend = "BULL"
-    elif bear_stack and all_slopes_neg:
+    elif bear_stack:
         trend = "BEAR"
 
     reasons.append(
